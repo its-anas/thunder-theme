2bbece3669386ff6a870e81f6cd6d4a56fdad5db
its-steve <itsbabayaga21@gmail.com>
2022-12-05 22:58:50 +0100

    Made search filter responsive, schema is the only thing left

--- assets/global-scripts.js
+++ assets/global-scripts.js
@@ -263,7 +263,7 @@ function hideLoadingBar() {
 	document.querySelector(".loading-bar").classList.add("end");
 	setTimeout(() => {
 		document.querySelector(".loading-bar").classList.remove("end");
-	}, 300);
+	}, 700);
 }
 
 // ANCHOR: Search drawer
@@ -570,6 +570,7 @@ function sendToCart(itemId, quantity) {
 	// if (document.querySelector("#add-to-cart-button")) {
 	// 	document.querySelector("#add-to-cart-button").classList.add("loading");
 	// }
+	showLoadingBar();
 
 	let variantId = parseInt(itemId);
 
@@ -597,6 +598,10 @@ function sendToCart(itemId, quantity) {
 					window.location.reload();
 				});
 			}
+			setTimeout(() => {
+				hideLoadingBar();
+			}, 1000);
+
 			return response.json();
 		})
 		.catch((error) => {
@@ -2910,3 +2915,156 @@ class FeaturedProductSlider extends FeaturedProduct {
 }
 
 customElements.define("featured-product-slider", FeaturedProductSlider);
+
+// ANCHOR: Search filter
+class FilterComponent extends HTMLElement {
+	constructor() {
+		super();
+	}
+	connectedCallback() {
+		this.injectSearchResults(this.buildUrl());
+		document.addEventListener("change", (event) => {
+			if (event.target.form) {
+				this.injectSearchResults(this.buildUrl());
+				showLoadingBar();
+				if (event.target.form.id === "filterForm") {
+					this.getNewResults(this.buildUrl());
+				}
+			}
+			if (event.target.className === "sort-by__select") {
+				showLoadingBar();
+				this.getNewResults(this.buildUrl());
+			}
+		});
+
+		document.addEventListener("click", (event) => {
+			if (event.target.className === "filter-active-filters__remove small" || event.target.className === "filter-active-filters__clear small underline") {
+				showLoadingBar();
+				this.getNewResults(event.target.dataset.url);
+			}
+		});
+	}
+
+	injectSearchResults(url) {
+		fetch(url)
+			.then((response) => response.text())
+			.then((data) => {
+				const parser = new DOMParser();
+				const doc = parser.parseFromString(data, "text/html");
+				const searchResults = doc.querySelector(".search-results-hidden");
+				document.querySelector(".search__page-tab[for='products']").innerHTML = "Products " + searchResults.innerHTML;
+			});
+	}
+
+	buildUrl() {
+		let parameters = [];
+		let windowLocation = window.location;
+		let searchTerm = `search?q=${document.querySelector(".search-section__input").value}`;
+		let sortBy = `&sort_by=${document.querySelector(".sort-by__select").value}`;
+		let url = `${windowLocation.origin}/${searchTerm}${sortBy}&type=product`;
+
+		this.querySelectorAll(".filter-group__container input").forEach((container) => {
+			if (container.type === "checkbox" && container.checked) {
+				parameters.push(container.name + "=" + container.value.replaceAll(" ", "+"));
+			}
+		});
+
+		let priceRangeLeft = this.querySelector(".filter-group__price-range__slider .range-min");
+		let priceRangeRight = this.querySelector(".filter-group__price-range__slider .range-max");
+
+		if (priceRangeLeft.value !== priceRangeLeft.min || priceRangeRight.value !== priceRangeRight.max) {
+			parameters.push(priceRangeLeft.name + "=" + priceRangeLeft.value);
+			parameters.push(priceRangeRight.name + "=" + priceRangeRight.value);
+		}
+
+		parameters.forEach((parameter) => {
+			url += "&" + parameter;
+		});
+
+		return url;
+	}
+
+	getNewResults(url) {
+		fetch(url)
+			.then((response) => response.text())
+			.then((data) => {
+				const parser = new DOMParser();
+				const doc = parser.parseFromString(data, "text/html");
+
+				const filterField = doc.querySelector("#filterForm");
+				document.querySelector("#filterForm").innerHTML = `${filterField.innerHTML}`;
+
+				const filters = doc.querySelector(".filter-active-filters");
+				document.querySelector(".filter-active-filters").innerHTML = `${filters.innerHTML}`;
+
+				const products = doc.querySelector(".search__results--list");
+				if (products.children.length > 0) {
+					document.querySelector(".search__results--list").innerHTML = `${products.innerHTML}`;
+				} else {
+					document.querySelector(".search__results--list").innerHTML = "<p>No results</p>";
+				}
+
+				history.replaceState(null, null, url);
+				hideLoadingBar();
+			});
+	}
+}
+customElements.define("filter-component", FilterComponent);
+
+class FilterPriceRange extends HTMLElement {
+	constructor() {
+		super();
+	}
+	connectedCallback() {
+		this.runRange();
+	}
+
+	runRange() {
+		let rangeInput = this.querySelectorAll(".filter-group__price-range__slider input");
+		let priceInput = this.querySelectorAll(".filter-group__price-range__from-input input");
+		let range = this.querySelector(".filter-group__price-range__slider .progress");
+		let priceGap = 1;
+
+		priceInput.forEach((input) => {
+			input.addEventListener("input", (e) => {
+				let minPrice = parseInt(priceInput[0].value);
+				let maxPrice = parseInt(priceInput[1].value);
+
+				if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
+					if (e.target.className === "input-min") {
+						rangeInput[0].value = minPrice;
+						range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
+					} else {
+						rangeInput[1].value = maxPrice;
+						range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+					}
+				}
+			});
+		});
+
+		rangeInput.forEach((input) => {
+			input.addEventListener("input", (e) => {
+				let minVal = parseInt(rangeInput[0].value),
+					maxVal = parseInt(rangeInput[1].value);
+
+				if (maxVal - minVal < priceGap) {
+					if (e.target.className === "range-min") {
+						rangeInput[0].value = maxVal - priceGap;
+					} else {
+						rangeInput[1].value = minVal + priceGap;
+					}
+				} else {
+					priceInput[0].value = minVal;
+					priceInput[1].value = maxVal;
+					range.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+					range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+				}
+			});
+		});
+	}
+}
+customElements.define("filter-price-range", FilterPriceRange);
+
+window.addEventListener("load", () => {
+	document.querySelector(".search__content--side").style.height = `calc(100% - ${document.querySelector(".header-section").offsetHeight}px + 8px)`;
+});
--- assets/main-search.js
+++ assets/main-search.js
@@ -1,23 +1,56 @@
-if (document.getElementById("search__results__filter-control")) {
-	document.getElementById("search__results__filter-control").addEventListener("click", () => {
-		let filterText = document.querySelector(".search__results__filter-text");
-		if (filterText.innerHTML === "Hide filter") {
-			filterText.innerHTML = "Show filter";
-		} else {
-			filterText.innerHTML = "Hide filter";
-		}
-		document.querySelector(".search__content--side").classList.toggle("collapse");
-	});
+class SearchPageTabs extends HTMLElement {
+	constructor() {
+		super();
+	}
+	connectedCallback() {
+		this.injectArticles();
+
+		this.addEventListener("click", (event) => {
+			let type = event.target.attributes.for.nodeValue;
+			this.querySelector(".search__page-tab.selected").classList.remove("selected");
+			event.target.classList.add("selected");
+
+			document.querySelector(".tab.active").classList.add("hidden");
+			document.querySelector(".tab.active").classList.remove("active");
+			setTimeout(() => {
+				document.querySelector(`.tab[type="${type}"]`).classList.remove("hidden");
+				document.querySelector(`.tab[type="${type}"]`).classList.add("active");
+			}, 300);
+
+			document.querySelector(".search__content").style.height = document.querySelector(`.tab[type="${type}"]`).offsetHeight + "px";
+		});
+	}
+
+	injectArticles() {
+		let windowLocation = window.location;
+		let searchTerm = `search?q=${document.querySelector(".search-section__input").value}`;
+		let articlesUrl = `${windowLocation.origin}/${searchTerm}&type=article`;
+
+		fetch(articlesUrl)
+			.then((response) => response.text())
+			.then((data) => {
+				const parser = new DOMParser();
+				const doc = parser.parseFromString(data, "text/html");
+
+				const searchResults = doc.querySelector(".search-results-hidden");
+				document.querySelector(".search__page-tab[for='articles']").innerHTML += ` ${searchResults.innerHTML}`;
+
+				const articles = doc.querySelector(".search__results--list[type='articles']");
+				document.querySelector(".search__results--list[type='articles']").innerHTML = `${articles.innerHTML}`;
+			});
+	}
 }
+customElements.define("search-page-tabs", SearchPageTabs);
 
-// if (document.querySelector(".search__results--head .sort-by__select")) {
-// 	document.querySelector(".search__results--head .sort-by__select").addEventListener("change", (e) => {
-// 		let value = e.target.value;
-
-// 		if (window.location.href.includes("&sort_by")) {
-// 			window.location.href = window.location.href.split("&sort_by")[0] + "&sort_by=" + value;
-// 		} else {
-// 			window.location.href = window.location.href + "&sort_by=" + value;
-// 		}
-// 	});
-// }
+document.querySelector(".show-filter").addEventListener("click", (event) => {
+	let filter = document.querySelector(".search__content--side");
+	filter.classList.toggle("active");
+});
+document.querySelector(".filter-close").addEventListener("click", (event) => {
+	let filter = document.querySelector(".search__content--side");
+	filter.classList.remove("active");
+});
+document.querySelector(".filter-mobile__button").addEventListener("click", (event) => {
+	let filter = document.querySelector(".search__content--side");
+	filter.classList.remove("active");
+});
